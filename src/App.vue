0f6f7b0d3dfd7822dc9fdff905f9a3cb85ab7a56
Ralle1968 <graef.ralf@gmail.com>
2018-03-15 23:32:15 +0100

    init commit
    Signed-off-by: Ralle1968 <graef.ralf@gmail.com>

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-navigation-drawer temporary v-model="sideNav" fixed>
+      <v-list>
+         <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>list</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Liste</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>EinkaufsListe</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>list</v-icon>
+          Liste</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main></main> 
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    }
+  }
+</script>
